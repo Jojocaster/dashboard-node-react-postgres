4bf70789148822b9d33ae7ce266f020c81ead04c
Joel Beaudon <joel.beaudon@gmail.com>
2017-12-13 20:00:28 +0000

    New animations, tasks improved, AnimationHeight added

--- app/src/components/TodoPage/task.js
+++ app/src/components/TodoPage/task.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import Moment from 'react-moment';
 import classNames from 'classnames/bind';
+import AnimateHeight from 'react-animate-height';
 
 import styles from './task.scss';
-let cx = classNames.bind(styles);
+const cx = classNames.bind(styles);
 
-const Task = ({task, removeTask, toggleTask, i}) => {
-  let className = cx({
-    task: true,
-    active: task.complete
-  });
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
 
-  return (
-    <span onClick={toggleTask} className={className}>
-      <span className={styles.todo}>{task.todoId}</span>
-      <span className={styles.title}>{task.title}</span>
-      <span className={styles.date}>
-        <Moment className={styles.day} format="DD">{task.due}</Moment>
-        <Moment className={styles.month} format="MMMM 'YY">{task.due}</Moment>
+    this.toggleState = this.toggleState.bind(this);
+  }
+
+  toggleState() {
+    const {task} = this.props;
+    if(!task.content) return;
+
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  render() {
+    const {task, removeTask, toggleTask, i} = this.props;
+    const {open} = this.state;
+
+    const taskClassName = cx({task: true, active: task.complete});
+    const contentClassName = cx({content: true, contentOpen: open});
+
+    return(
+      <span onClick={this.toggleState} className={taskClassName}>
+        <span className={styles.todo}>{task.todoId}</span>
+        <span className={styles.title}>{task.title}</span>
+        <span className={styles.date}>
+          <Moment className={styles.day} format="DD">{task.due}</Moment>
+          <Moment className={styles.month} format="MMM 'YY">{task.due}</Moment>
+        </span>
+        <AnimateHeight contentClassName={styles.contentWrapper} duration={200} height={open ? "auto" : 0}>
+          <div className={contentClassName}>{task.content}</div>
+        </AnimateHeight>
       </span>
-    </span>
-  )
+    )
+  }
+}
+
+// const Task = ({task, removeTask, toggleTask, i}) => {
+
+
+//   return (
+//     <span onClick={toggleTask} className={className}>
+//       <span className={styles.todo}>{task.todoId}</span>
+//       <span className={styles.title}>{task.title}</span>
+//       <span className={styles.date}>
+//         <Moment className={styles.day} format="DD">{task.due}</Moment>
+//         <Moment className={styles.month} format="MMM 'YY">{task.due}</Moment>
+//       </span>
+//     </span>
+//   )
   // <Card className={task.complete ? 'task active' : 'task'}>
   //   <CardContent>
   //     <Typography type="body1">{task.title}</Typography>
@@ -33,6 +71,6 @@ const Task = ({task, removeTask, toggleTask, i}) => {
   //   </CardContent>
   // </Card>
   // <div onClick={onTaskClick}>{task.content}</div>
-}
+// }
 
 export default Task;
